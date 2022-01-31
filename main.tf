0b04f914d7f11453c3ae871e04339f3ebc40d109
gopal-kannan <95870198+gopal-kannan@users.noreply.github.com>
2022-01-31 16:42:54 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,19 +5,16 @@ locals {
     Product      = "SAEB Platform"
     ProductOwner = "Shaun Laughland"
     CreatedBy    = "Terraform"
+	rg_name      = "SAEB-DataAnalytics"
+	rg_location  = var.location 
   }
-}
-
-resource "azurerm_resource_group" "saeb" {
-  name     = "SAEB-DataAnalytics"
-  location = var.location 
 }
 
 # Create ADF
 resource "azurerm_data_factory" "saeb_adf" {
   name                = "adf-saeb-dev-01-${random_string.random.result}"
-  location            = azurerm_resource_group.saeb.location
-  resource_group_name = azurerm_resource_group.saeb.name
+  location            = local.location
+  resource_group_name = local.name
 }
 
 resource "random_string" "random" {
@@ -30,8 +27,8 @@ resource "random_string" "random" {
 # Create storage account for main input and curated CSV files as well as for synapse filesystem
 resource "azurerm_storage_account" "saeb_storage" {
   name                     = "stsaebdevca01${random_string.random.result}"
-  resource_group_name      = azurerm_resource_group.saeb.name
-  location                 = azurerm_resource_group.saeb.location
+  resource_group_name      = local.name
+  location                 = local.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
@@ -54,8 +51,8 @@ resource "azurerm_storage_container" "saeb_storage_container" {
 # Create Databricks workspace and clusters and associate with a repo
 resource "azurerm_databricks_workspace" "saeb_databricks_workspace" {
   name                = "dbw-saeb-dev-01-${random_string.random.result}"
-  resource_group_name = azurerm_resource_group.saeb.name
-  location            = azurerm_resource_group.saeb.location
+  resource_group_name = local.name
+  location            = local.location
   sku                 = "standard"
 
   tags = local.common_tags
@@ -70,16 +67,16 @@ resource "databricks_repo" "saeb_databricks_repo" {
 # Create logic app + storage account + app service plan that all work together
 resource "azurerm_storage_account" "saeb_logic_app_storage" {
   name                     = "salogicappdevca02${random_string.random.result}"
-  resource_group_name      = azurerm_resource_group.saeb.name
-  location                 = azurerm_resource_group.saeb.location
+  resource_group_name      = local.name
+  location                 = local.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_app_service_plan" "saeb_service_plan" {
   name                = "asp-saeb-dev-02-${random_string.random.result}"
-  location            = azurerm_resource_group.saeb.location
-  resource_group_name = azurerm_resource_group.saeb.name
+  location            = local.location
+  resource_group_name = local.name
 
   sku {
     tier = "WorkflowStandard"
@@ -89,8 +86,8 @@ resource "azurerm_app_service_plan" "saeb_service_plan" {
 
 resource "azurerm_logic_app_standard" "saeb_logic_app" {
   name                       = "logic-saeb-dev-01-${random_string.random.result}"
-  location                   = azurerm_resource_group.saeb.location
-  resource_group_name        = azurerm_resource_group.saeb.name
+  location                   = local.location
+  resource_group_name        = local.name
   app_service_plan_id        = azurerm_app_service_plan.saeb_service_plan.id
   storage_account_name       = azurerm_storage_account.saeb_logic_app_storage.name
   storage_account_access_key = azurerm_storage_account.saeb_logic_app_storage.primary_access_key
@@ -99,8 +96,8 @@ resource "azurerm_logic_app_standard" "saeb_logic_app" {
 # Create Synapse workspace with dedicated SQL Pool
 resource "azurerm_synapse_workspace" "saeb_synapse" {
   name                                 = "synw-saeb-dev-01-${random_string.random.result}"
-  resource_group_name                  = azurerm_resource_group.saeb.name
-  location                             = azurerm_resource_group.saeb.location
+  resource_group_name                  = local.name
+  location                             = local.location
   storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.saeb_data_lake_fs.id
   sql_administrator_login              = "sqladminuser"
   sql_administrator_login_password     = "H@Sh1CoR3!"
