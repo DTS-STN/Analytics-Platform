b5ece8794cc74bbc90b9148434106c5485ac8333
Alex Soloviev <alex.soloviev@gmail.com>
2022-02-09 23:11:11 +0000

    initial key vault commit

--- main.tf
+++ main.tf
@@ -115,3 +115,37 @@ resource "azurerm_synapse_sql_pool" "saeb_synapse_sqlpool" {
   sku_name             = "DW100c"
   create_mode          = "Default"
 }
+
+# Key vault
+
+# Reference to client configuration in order to access tenant ID and subscription ID
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "saeb_keyvault" {
+  name                        = "kv-saeb-dev-01"
+  location                    = "${data.azurerm_resource_group.saeb.location}"
+  resource_group_name         = azurerm_resource_group.saeb.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id # replace this with service principal
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
