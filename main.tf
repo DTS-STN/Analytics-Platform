25ef6a5791b15edae009fb240809c8cc00251695
Alex Soloviev <alex.soloviev@gmail.com>
2022-02-17 17:56:40 +0000

    deleted unnecessary block

--- main.tf
+++ main.tf
@@ -160,34 +160,6 @@ resource "azurerm_key_vault_access_policy" "default_policy" {
 #   storage_permissions = var.kv-storage-permissions-read
 # }
 
-
-
-access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "create",
-      "get",
-      "list"
-    ]
-
-    secret_permissions = [
-      "set",
-      "get",
-      "delete",
-      "purge",
-      "recover",
-      "list"
-    ]
-
-    storage_permissions = [
-      "get",
-    ]
-  }
-
-
-
 resource "azurerm_key_vault_secret" "saeb_test_secret" {
   name         = "secret-sauce"
   value        = "szechuan pass"
